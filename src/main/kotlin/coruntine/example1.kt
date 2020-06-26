dc83797cf092207aa2bc098d02a4d633109d62e2
mingjun-wu <511309453@qq.com>
2020-06-26 09:28:55 +0800

    init commit

--- src/main/kotlin/coruntine/example1.kt
+++ src/main/kotlin/coruntine/example1.kt
@@ -2,17 +2,21 @@ package coruntine
 
 import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.awaitEvent
+import io.vertx.kotlin.coroutines.awaitResult
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
 
 suspend fun main() {
     val vertx = Vertx.vertx()
+    val log = LoggerFactory.getLogger("my log")
 
     /**
      * The vertx.dispatcher() returns a coroutine dispatcher that
      * execute coroutines using the Vert.x event loop.
      */
+    log.info("1")
     GlobalScope.launch(vertx.dispatcher()) {
         /**
          * The awaitEvent function suspends the execution of the coroutine until
@@ -20,8 +24,10 @@ suspend fun main() {
          * was given to the handler.
          */
         val timerId = awaitEvent<Long> { handler->
+            log.info("2")
             vertx.setTimer(1000,handler)
         }
-        println("Event fired from timer with id $timerId")
+        log.info("Event fired from timer with id $timerId")
     }
+    log.info("3")
 }
